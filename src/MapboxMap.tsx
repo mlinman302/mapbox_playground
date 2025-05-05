82ea9fcaf02ae38682933a0e11eaa8c7ae4675c5
mlinman302 <max.inman101@gmail.com>
2025-05-05 03:03:04 -0400

    faulkner done

--- src/MapboxMap.tsx
+++ src/MapboxMap.tsx
@@ -40,49 +40,49 @@ const tempNodes: tempNode[] = [
         floor: 1,
         kind: "elevator"
     },
-    {
-        lat: 42.09324032652947,
-        long: -71.26658286746988,
-        floor: 2,
-        kind: "elevator"
-    },
-    {
-        lat: 42.09277375925052,
-        long: -71.26599086652641,
-        floor: 2,
-        kind: "inter"
-    },
-    {
-        lat: 42.09248682590311,
-        long: -71.26629410633672,
-        floor: 2,
-        kind: "elevator"
-    },
-    {
-        lat: 42.09248682590311,
-        long: -71.26629410633672,
-        floor: 3,
-        kind: "elevator"
-    },
-
-    {
-        lat: 42.09277375925052,
-        long: -71.26599086652641,
-        floor: 3,
-        kind: "inter"
-    },
-    {
-        lat: 42.09324032652947,
-        long: -71.26658286746988,
-        floor: 3,
-        kind: "poi"
-    },
-    {
-        lat: 42.09324032652947,
-        long: -71.26658286746988,
-        floor: 4,
-        kind: "poi"
-    },
+    // {
+    //     lat: 42.09324032652947,
+    //     long: -71.26658286746988,
+    //     floor: 2,
+    //     kind: "elevator"
+    // },
+    // {
+    //     lat: 42.09277375925052,
+    //     long: -71.26599086652641,
+    //     floor: 2,
+    //     kind: "inter"
+    // },
+    // {
+    //     lat: 42.09248682590311,
+    //     long: -71.26629410633672,
+    //     floor: 2,
+    //     kind: "elevator"
+    // },
+    // {
+    //     lat: 42.09248682590311,
+    //     long: -71.26629410633672,
+    //     floor: 3,
+    //     kind: "elevator"
+    // },
+    //
+    // {
+    //     lat: 42.09277375925052,
+    //     long: -71.26599086652641,
+    //     floor: 3,
+    //     kind: "inter"
+    // },
+    // {
+    //     lat: 42.09324032652947,
+    //     long: -71.26658286746988,
+    //     floor: 3,
+    //     kind: "poi"
+    // },
+    // {
+    //     lat: 42.09324032652947,
+    //     long: -71.26658286746988,
+    //     floor: 4,
+    //     kind: "poi"
+    // },
 
 ]
 
@@ -116,27 +116,27 @@ export type buildingAttributes = {
 //     nodes: tempNodes,
 // }
 
-const Pat22SceneCoords: LngLatLike = [-71.26696722883923, 42.09258410491776]
-const Pat22BuildingCoords: LngLatLike = [-71.26697223199403, 42.09223043183033]
-const Pat22BuildingMaskCoords: LngLatLike = [-71.26629497632113, 42.09248760267727]
-const Pat22ImageConstants: imageConstants = {
-    width: 68,
-    height: 87,
-    offsetEast: -2,
-    offsetNorth: 5
-}
-const Patriot22Building = {
-    sceneCoords: Pat22SceneCoords,
-    buildingCoords: Pat22BuildingCoords,
-    buildingPaths: ["/22Patriot/Pat22Floor.gltf", "/22Patriot/Pat22Floor.gltf", "/22Patriot/Pat22Floor.gltf", "/22Patriot/Pat22Floor.gltf"],
-    buildingMaskPath: "/20Patriot/PatExterior.glb",
-    buildingRotation: -Math.PI/10,
-    floorHeight: 20,
-    buildingMaskCoords: Pat22BuildingMaskCoords,
-    floorPlanPaths: ['22Patriot/22PatFloor1.png', '22Patriot/22PatFloor2.png', "22Patriot/22PatFloor3.png", "22Patriot/22PatFloor4.png"],
-    nodes: tempNodes,
-    imageConstants: Pat22ImageConstants
-}
+// const Pat22SceneCoords: LngLatLike = [-71.26696722883923, 42.09258410491776]
+// const Pat22BuildingCoords: LngLatLike = [-71.26697223199403, 42.09223043183033]
+// const Pat22BuildingMaskCoords: LngLatLike = [-71.26629497632113, 42.09248760267727]
+// const Pat22ImageConstants: imageConstants = {
+//     width: 68,
+//     height: 87,
+//     offsetEast: -2,
+//     offsetNorth: 5
+// }
+// const Patriot22Building = {
+//     sceneCoords: Pat22SceneCoords,
+//     buildingCoords: Pat22BuildingCoords,
+//     buildingPaths: ["/22Patriot/Pat22Floor.gltf", "/22Patriot/Pat22Floor.gltf", "/22Patriot/Pat22Floor.gltf", "/22Patriot/Pat22Floor.gltf"],
+//     buildingMaskPath: "/20Patriot/PatExterior.glb",
+//     buildingRotation: -Math.PI/10,
+//     floorHeight: 20,
+//     buildingMaskCoords: Pat22BuildingMaskCoords,
+//     floorPlanPaths: ['22Patriot/22PatFloor1.png', '22Patriot/22PatFloor2.png', "22Patriot/22PatFloor3.png", "22Patriot/22PatFloor4.png"],
+//     nodes: tempNodes,
+//     imageConstants: Pat22ImageConstants
+// }
 
 // const MainSceneCoords: LngLatLike = [-71.106549430016, 42.335842853824396]
 // const MainBuildingCoords: LngLatLike = [-71.10636459548073, 42.33526357549587]
@@ -153,20 +153,27 @@ const Patriot22Building = {
 //     nodes: tempNodes,
 // }
 
-// const FaulknerSceneCoords: LngLatLike = [-71.12834142530612, 42.30150822410094]
-// const FaulknerBuildingCoords: LngLatLike = [-71.12855652822122, 42.300928445283546]
-// const FaulknerBuildingMaskCoords: LngLatLike = [-71.12855652822122, 42.300928445283546]
-// const FaulknerBuilding = {
-//     sceneCoords: FaulknerSceneCoords,
-//     buildingCoords: FaulknerBuildingCoords,
-//     buildingPaths: ["/Faulkner/FaulknerFloor1.gltf"], // one per floor
-//     buildingMaskPath: "/Faulkner/FaulknerExterior.gltf",
-//     buildingRotation: 0,
-//     floorHeight: 45,
-//     buildingMaskCoords: FaulknerBuildingMaskCoords,
-//     floorPlanPaths: [''],
-//     nodes: tempNodes,
-// }
+const FaulknerSceneCoords: LngLatLike = [-71.12834142530612, 42.30150822410094]
+const FaulknerBuildingCoords: LngLatLike = [-71.12855652822122, 42.300928445283546]
+const FaulknerBuildingMaskCoords: LngLatLike = [-71.12855652822122, 42.300928445283546]
+const FaulknerImageConstants: imageConstants = {
+    width: 223,
+    height: 170,
+    offsetEast: 23,
+    offsetNorth: 13
+}
+const FaulknerBuilding: buildingAttributes = {
+    sceneCoords: FaulknerSceneCoords,
+    buildingCoords: FaulknerBuildingCoords,
+    buildingPaths: ["/Faulkner/FaulknerFloor1.gltf"], // one per floor
+    buildingMaskPath: "/Faulkner/FaulknerExterior.gltf",
+    buildingRotation: 0,
+    floorHeight: 45,
+    buildingMaskCoords: FaulknerBuildingMaskCoords,
+    floorPlanPaths: ['Faulkner/FaulknerFloor1.png'],
+    nodes: tempNodes,
+    imageConstants: FaulknerImageConstants
+}
 
 // const ChestnutSceneCoords: LngLatLike = [-71.14974760810384, 42.325950820451]
 // const ChestnutBuildingCoords: LngLatLike = [-71.1500853668773, 42.325693309988054]
@@ -200,13 +207,13 @@ const MapboxMap: React.FC = () => {
         const map: Map = new mapboxgl.Map({
             container: mapContainer.current as HTMLDivElement,
             style: "mapbox://styles/mapbox/light-v11",
-            center: Pat22SceneCoords,
+            center: FaulknerSceneCoords,
             zoom: 18,
             pitch: 55,
             antialias: true,
         });
 
-        CreateLayer(map, Patriot22Building)
+        CreateLayer(map, FaulknerBuilding)
 
         return () => map.remove();
     }, []);
